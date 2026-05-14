c48a9848dc14cdc40dfca9d0be985fec1a9e2288
Samara <147871912+slandai2@users.noreply.github.com>
2026-05-14 10:23:57 -0400

    Update script.js
    Added notes on code

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+// Target data for block timer and play timer respectively
 const openTimer = document.querySelectorAll('[data-modal-target]')
 const openOtherTimer = document.querySelectorAll('[data-modal-target-time]')
 const testVar = document.querySelectorAll('[data-modal-target-time]')
+// Target data to apply overlay to block timer and play timer
 const overlay = document.getElementById('overlay')
 const overlayTime = document.getElementById('overlayTime')
 
 var timerRan = 'false';
 var playing = 'false';
 
-var countdown = new Date().getTime() + 3600000; // Note for Samara - annotate  
+var countdown = new Date().getTime() + 3600000;   
 
+// Checks to open block timer
 openTimer.forEach(div => { 
+    // When site is opened, open block timer on load if the play timer ran
     window.addEventListener('load', () => {
-            const saved = localStorage.getItem('timerStatus')
-            if (saved === 'true') {
-                const modal = div.closest('.modal')
-                countdown = parseInt(localStorage.getItem("time"))
-                openModal(modal)
-            }
-            
+        const saved = localStorage.getItem('timerStatus')
+        if (saved === 'true') {
+            const modal = div.closest('.modal')
+            countdown = parseInt(localStorage.getItem("time"))
+            openModal(modal)
+        } 
     })
 })
 
+// hecks to open play timer
 openOtherTimer.forEach(button => {
+    // When user clicks confirm, close the pop up
     button.addEventListener('click', () => {
         const t = button.closest('.modalTime')
         confirmed(t)
     })
 })
 
+// When opening game page, check to open pop up to set play timer
 testVar.forEach(div => {
     window.addEventListener('load', () => {
+        // If the time has been confirmed, begin play timer
         const confirmTime = localStorage.getItem('confirmedTime')
         const x = div.closest('.modalTime')
         if (confirmTime === 'true') {
@@ -41,11 +48,14 @@ testVar.forEach(div => {
     })
 })
 
+// Runs block timer
 function runInterval() {
+    // If "time" value isn't set, set to 1 hour
     if (!localStorage.getItem("time")) {
         localStorage.setItem("time", countdown);
     }
 
+    // Interval to run block countdown
     x = setInterval(function () {
         let now = new Date().getTime()
         let distance = countdown - now;
@@ -64,10 +74,12 @@ function runInterval() {
 
             
     }, 1000)
-    
+
+    // Reset time
     localStorage.setItem("time", new Date().getTime() + 3600000)
 }
 
+// Open block timer and run the countdown
 function openModal(modal) {
     if (modal == null) return false
     modal.classList.add('active')
@@ -75,6 +87,7 @@ function openModal(modal) {
     runInterval() 
 }
 
+// Close block timer
 function closeModal(modal) {
     if (modal == null) return
     clearInterval(x)
@@ -90,6 +103,7 @@ function closeModal(modal) {
     
 }
 
+// Confirm user set play time and begin timer
 function confirmed(t) {
     if (t == null) return
     
@@ -100,8 +114,6 @@ function confirmed(t) {
     const totalMs = (hour * 3600000) + (min * 60000) + (sec * 1000);
     var endTime = new Date().getTime() + totalMs;
 
-    // localStorage.setItem('timerStatus', timerRan);
-    // localStorage.setItem('timerEndTime', endTime);
     localStorage.setItem('confirmedTime', 'true');
 
     timerCountdown(endTime);
